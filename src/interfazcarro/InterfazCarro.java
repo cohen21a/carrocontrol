c56c89efa6bb4ce9f85e9a4f4acd2a87638883d6
a <a@192.168.1.30>
2018-06-03 10:44:03 -0500

    se comento todoslos test  menos suma  y resta esto con el fin de generar un fallo  en el test  de resta y se agrega un documento en word  donde se explica todo el proceso de creacion del repositorio, como  hacer una copia  desde github a nustro escritorio con clone y  describe las diferencias vistas entre cada modificacion.

---
+++ src/interfazcarro/InterfazCarro.java
@@ -0,0 +1,16 @@
+package interfazcarro;
+
+/**
+ *
+ * @author jesusgabriel
+ */
+public class InterfazCarro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Interfaz().setVisible(true);
+    }
+    
+}
